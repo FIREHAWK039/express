85ca7b44a5cee3fca7591e6782ca1196dd8bc0fe
firehawk039 <firehawk039@gmail.com>
2026-06-01 01:30:50 +0530

    data push

--- p1express/index.js
+++ p1express/index.js
@@ -7,6 +7,10 @@ const app= express()
 const port = 8000
 //mobile ke liye taki  jason data na jaye
 
+const fs = require("fs")
+
+app.use(express.urlencoded({extended: false }))
+
 app.get("/users",(req, response)=>{
     const html = users.map((user)=>{
         return `<ul>${user.first_name} ${user.last_name}</ul>`
@@ -16,14 +20,33 @@ app.get("/users",(req, response)=>{
 )
 
 //yhan se rest api cahlu h
-app.get("/api/users", (req, response)=>{
-return response.json(users)
+app
+.route("/api/users/:id")
+.get((req, response)=>{
+const id = Number(req.params.id)
+const user =users.find(user => user.id ===id)
+return response.json(user)
+})
+.patch((req, response)=>{
+        //edit your user
+        return response.json({status: "pending"})
+})
+.delete((req, response)=>{
+        //delete your user
+        return response.json({status: "pending"})
 })
 
-app.get("/api/users/:id", (req, response)=>{
-    const id = Number(req.params.id)
-    const user = users.find((user)=> user.id === id)
-    return response.json(user)
-})
 
+app
+.route("/api/users")
+
+.post((req, response)=>{
+        const body = req.body
+        users.push({...body, id: users.length + 1})
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+            return response.json({status: "success", id: users.length})
+        })
+        
+})
+ 
 app.listen(port,()=> console.log("servver bhag raha hai"))
